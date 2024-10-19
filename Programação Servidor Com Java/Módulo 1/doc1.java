d255ea97b2e14f5937444ef3bc8aa4c1a03103e6
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2024-10-18 21:18:59 -0300

    update doc1.java

--- Programação Servidor Com Java/Módulo 1/doc1.java
+++ Programação Servidor Com Java/Módulo 1/doc1.java
@@ -3,3 +3,30 @@
 int a = Integer.parseInt(request.getParameter("valor"));
 response.getWriter().println("<html><body>Valor Enviado: "+a+
                              "</body></html>");
+
+
+// No modelo desktop, temos a armazenagem de objetos em um espaço de memória local, permitindo que os dados sejam mantidos durante toda a execução do sistema, mesmo que ocorra a troca de janelas, o que viabiliza a manutenção de estados, enquanto no modelo Web, a cada requisição e resposta, todos os objetos são recriados e o estado não pode ser mantido.
+
+
+//Uma solução primária para a manutenção de estados no HTTP é o uso de cookies, algo que pode ser explorado por meio do Java Script.
+
+
+function setUser(login) {
+  var d = new Date();
+  d.setTime(d.getTime() + (24 * 3600 * 1000)); // 24 horas
+  document.cookie = "username=" + login + ";expires=" +
+                    d.toUTCString() + ";path=/";
+}
+function getUSer() {
+  var userStr = "username=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(usrStr) == 0)
+      return c.substring(usrStr.length, c.length);
+  }
+  return "";
+}
+
+
+//Nas funções de exemplo, com sintaxe Java Script, temos o uso do atributo cookie, no objeto document, para armazenar o usuário logado de forma local. O momento para expiração da informação é calculado em milissegundos, sendo definido um prazo total de 24 horas, a partir do qual teremos a invalidação do cookie.
